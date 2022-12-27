d995c6386f8afa697ab7f651dde77fcd3dcb5342
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-12-27 15:06:25 -0800

    data validation

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -17,7 +17,9 @@ function getPizza() {
   // get pizzaInfo
   fetch(`/api/pizzas/${pizzaId}`)
     .then(response => {
+      console.log(response);
       if (!response.ok) {
+        console.log('hi');
         throw new Error({ message: 'Something went wrong!' });
       }
 
@@ -30,7 +32,6 @@ function getPizza() {
       window.history.back();
     });
 }
-
 
 function printPizza(pizzaData) {
   console.log(pizzaData);
@@ -63,12 +64,14 @@ function printComment(comment) {
       <h5 class="text-dark">${comment.writtenBy} commented on ${comment.createdAt}:</h5>
       <p>${comment.commentBody}</p>
       <div class="bg-dark ml-3 p-2 rounded" >
-        ${comment.replies && comment.replies.length
-      ? `<h5>${comment.replies.length} ${comment.replies.length === 1 ? 'Reply' : 'Replies'
-      }</h5>
+        ${
+          comment.replies && comment.replies.length
+            ? `<h5>${comment.replies.length} ${
+                comment.replies.length === 1 ? 'Reply' : 'Replies'
+              }</h5>
         ${comment.replies.map(printReply).join('')}`
-      : '<h5 class="p-1">No replies yet!</h5>'
-    }
+            : '<h5 class="p-1">No replies yet!</h5>'
+        }
       </div>
       <form class="reply-form mt-3" data-commentid='${comment._id}'>
         <div class="mb-3">
@@ -125,7 +128,7 @@ function handleNewCommentSubmit(event) {
     })
     .then(commentResponse => {
       console.log(commentResponse);
-      location.reload();
+      // location.reload();
     })
     .catch(err => {
       console.log(err);
@@ -173,7 +176,7 @@ function handleNewReplySubmit(event) {
     });
 }
 
-$backBtn.addEventListener('click', function () {
+$backBtn.addEventListener('click', function() {
   window.history.back();
 });
 
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,9 @@
-// import methods into routes
 const router = require('express').Router();
 const {
-    addComment,
-    removeComment,
-    addReply,
-    removeReply
+  addComment,
+  removeComment,
+  addReply,
+  removeReply
 } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
@@ -12,10 +11,11 @@ router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
 router
-    .route('/:pizzaId/:commentId')
-    .put(addReply)
-    .delete(removeComment)
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
 
+// /api/comments/<pizzaId>/<commentId>/<replyId>
 router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
